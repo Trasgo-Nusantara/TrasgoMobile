36addc0fabb4c07ad654b039f346f150788efdf5
hilmanzu <hilmanzutech@gmail.com>
2025-04-13 12:31:54 +0700

    feat:

--- src/screen/feature/trasfood/DetailFoodScreen.jsx
+++ src/screen/feature/trasfood/DetailFoodScreen.jsx
@@ -12,19 +12,18 @@ const { width } = Dimensions.get('window');
 const DetailFoodScreen = ({ navigation }) => {
 
     const [listPlace, setListPlace] = useState([])
-    if (listPlace.length === 0) {
-        return (
-            <View style={[COMPONENT_STYLES.container, { padding: 0, alignItems: 'center', justifyContent: 'center' }]}>
-                <StatusBar backgroundColor={COLORS.primary} barStyle="light-content" />
-                <Text style={[COMPONENT_STYLES.textMedium, { textAlign: 'center' }]}>daftar makanan belum tersedia</Text>
-            </View>
-        )
-    }
+    // if (listPlace.length === 0) {
+    //     return (
+    //         <View style={[COMPONENT_STYLES.container, { padding: 0, alignItems: 'center', justifyContent: 'center' }]}>
+    //             <StatusBar backgroundColor={COLORS.primary} barStyle="light-content" />
+    //             <Text style={[COMPONENT_STYLES.textMedium, { textAlign: 'center' }]}>daftar makanan belum tersedia</Text>
+    //         </View>
+    //     )
+    // }
 
     const getDatas = async (url) => {
         try {
             const response = await getData(`warung/+6281310531713`);
-            console.log(response.data.makanan)
             setListPlace(response.data.makanan);
         } catch (error) {
             console.error(error);
@@ -42,22 +41,23 @@ const DetailFoodScreen = ({ navigation }) => {
                 {listPlace.map((item, index) => (
                     <View key={index}>
                         <View style={styles.listItem}>
-                            <View style={styles.image}>
-                                <Image source={{ uri: item.imageCover }} style={{ resizeMode: 'cover', flex: 1, borderRadius: 10 }} />
-                            </View>
-                            <View style={{ marginLeft: 20, flex: 1 }}>
+                            <View style={{ marginRight: 20, flex: 1 }}>
                                 <Text style={[COMPONENT_STYLES.textMedium, { fontWeight: '400' }]} numberOfLines={1} ellipsizeMode="tail">{item.fullName}</Text>
                                 <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                    <Text style={[COMPONENT_STYLES.textSmallXL]}>Medium</Text>
-                                    <View style={{ width: 5, height: 5, backgroundColor: COLORS.primary, borderRadius: 100, marginHorizontal: 5 }} />
-                                    <Text style={[COMPONENT_STYLES.textSmallXL]}>Ayam dan Bebek</Text>
+                                    <Text style={[COMPONENT_STYLES.textSmallXL]}>{item.description}</Text>
                                 </View>
                                 <View style={COMPONENT_STYLES.spacerSmall} />
+                                <View style={COMPONENT_STYLES.spacer} />
                                 <View style={{ backgroundColor: COLORS.primary, height: 0.6 }} />
                                 <View style={COMPONENT_STYLES.spacerSmall} />
                                 <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                    <Text style={[COMPONENT_STYLES.textSmallXL]}>Ongkir 12rb</Text>
+                                    <Text style={[COMPONENT_STYLES.textLarge]}>
+                                        IDR {item.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}
+                                    </Text>
                                 </View>
+                            </View>
+                            <View style={styles.image}>
+                                <Image source={{ uri: item.imageCover }} style={{ resizeMode: 'cover', flex: 1, borderRadius: 10 }} />
                             </View>
                         </View>
                     </View>
@@ -77,8 +77,8 @@ const styles = StyleSheet.create({
         marginBottom: 10,
     },
     image: {
-        width: 80,
-        height: 80,
+        width: 120,
+        height: 120,
         borderRadius: 10,
     },
     barName: {
